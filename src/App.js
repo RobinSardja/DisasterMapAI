7eed0e286fb8200af9db99126b2648f50f39522b
GrassC575 <61118999+GrassC575@users.noreply.github.com>
2023-10-07 15:11:50 -0400

    create grid container for text and map component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+class TextBox extends React.Component{
+  render() {
+    return (
+      <div>
+        <p>Text here :D</p>
+      </div>
+    )
+  }
+}
+
+class MapBox extends React.Component{
+  render() {
+    return (
+      <div>
+        <p>Image here :0</p>
+        <a href="https://media.licdn.com/dms/image/D4D03AQEHfGQTbF6yyQ/profile-displayphoto-shrink_100_100/0/1665005596052?e=1701907200&v=beta&t=N4HM0TlTDBb-3Np32Kkf-Gv8LnTIE-BrDl-byS4bzWE">Click here</a>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="grid-container">
+          <TextBox />
+          <MapBox />
+        </div>
     </div>
   );
 }
