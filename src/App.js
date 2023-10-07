08a649378c36f93d8df10b0c33e82b4c3ce7eda6
Robin Sardja <robinsardja@gmail.com>
2023-10-07 17:56:42 -0400

    Cleared React boilerplate code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// const API_KEY = "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"
 
-function App() {
+// async function fetchData() {
+//   const response = await fetch( "https://api.openai.com/v1/chat/completions", {
+//     method: "POST",
+//     headers: {
+//       Authorization: `Bearer ${API_KEY}`,
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-3.5-turbo",
+//       messages: [{
+//         role: "user",
+//         content: "What is the worst thing about wildfires?",
+//       }],
+//       // max_tokens: 7,
+//     })
+//   })
+
+//   const data = await response.json()
+//   console.log(data)
+// }
+
+// fetchData()
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
-export default App;
+export default App
