8ab91bccbfb28340c8407e3b818176027025f594
Robin Sardja <robinsardja@gmail.com>
2023-10-08 01:59:03 -0400

    Initial working merge between chatbot and main.

---
+++ server.js
@@ -0,0 +1,36 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const API_KEY = process.env.GPT_API_KEY
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: "user",
+                content: `Summarize the latest updates about ${req.body.disaster} in ${req.body.county}, ${req.body.state} in 50 words or less`,
+            }],
+            max_tokens: 100,
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen( PORT, () => console.log('Your server is running on PORT ' + PORT) )
--- src/components/AIText.js
+++ src/components/AIText.js
@@ -1,13 +1,90 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
 
-class AIText extends React.Component{
-    render() {
-      return (
-        <div>
-          <p>Robin's super cool AI text here :D</p>
+const AIText = () => {
+
+  const [ value, setValue ] = useState("")
+  const [ message, setMessage ] = useState("")
+
+  const [ previousChats, setPreviousChats ] = useState([])
+
+  const [ currentTitle, setCurrentTitle ] = useState("")
+
+  const [ disaster, setDisaster ] = useState("Hurricane Idalia")
+  const [ county, setCounty ] = useState("Hillsborough County")
+  const [ state, setState ] = useState("FL")
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        disaster: disaster,
+        county: county,
+        state: state,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    setCurrentTitle(value)
+    setPreviousChats(prevChats => (
+      [ // ...prevChats,
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]
+    ))
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter( previousChat => previousChat.title === currentTitle )
+  const uniqueTitles = Array.from(new Set(previousChats.map( previousChat => previousChat.title )))
+  console.log(uniqueTitles)
+
+  return (
+    <div className="App">
+      <section className="main">
+        <h1>EmergenSAVE</h1>
+        <ul className="feed">
+          {currentChat.map(( chatMessage, index ) => <li key={index}>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <button id="submit" onClick={getMessages}>Latest info</button>
+          </div>
+          <p className="info">
+            Made by Grace Chang, Nathan Cheng, Robin Sardja, and Nicholas Tsai for SASEhack Fall 2023
+          </p>
         </div>
-      );
-    };
-};
+      </section>
+    </div>
+  );
+}
+
+// class AIText extends React.Component{
+//     render() {
+//       return (
+//         <div>
+//           <p>Robin's super cool AI text here :D</p>
+//         </div>
+//       );
+//     };
+// };
 
 export default AIText;
