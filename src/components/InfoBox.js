d1ad1968cd8e089ab4b707be151ba4b9cbf98dd8
GrassC575 <61118999+GrassC575@users.noreply.github.com>
2023-10-08 16:12:57 -0400

    ui changes

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -5,9 +5,9 @@ const InfoBox = ({info}) => {
         <div className="info-list">
           <p>Disaster Type: <strong>{info.id}</strong></p>
           <p>Name: <strong>{info.title}</strong></p>
-          <p>State: <strong>{info.state}</strong></p>
-          <p>County: <strong>{info.county}</strong></p>
-          <p>Date: <strong>{info.date}</strong></p>
+          {info.state && <p>State: <strong>{info.state}</strong></p>}
+          {info.county && <p>County: <strong>{info.county}</strong></p>}
+          {info.date && <p>Date: <strong>{info.date}</strong></p>}
         </div>
     </div>
   )
