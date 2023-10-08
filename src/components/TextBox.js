7518f8aac5be710cf0ef6055ae93b5374e2c22db
GrassC575 <61118999+GrassC575@users.noreply.github.com>
2023-10-08 12:09:33 -0400

    combine ai chatbox with map

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -4,7 +4,8 @@ class TextBox extends React.Component{
     render() {
       return (
         <div>
-          <p>Halloween-themed Pokémon are Pokémon that have creepy, spooky, or festive appearances or abilities that fit the Halloween mood. They include Ghost-type Pokémon, as well as some Dark and Psychic-type Pokémon. Some examples of Halloween-themed Pokémon are Gengar, Mismagius, Pumpkaboo, Mimikyu, and Polteageist. There are different ways to enjoy Halloween-themed Pokémon, such as building teams for battles or raids, collecting cards with special stamps, or decorating with statues and candy bowls.</p>
+          <h1>Disaster Map</h1>
+          <p>Click on an icon to learn more about a natural disaster in that area!</p>
         </div>
       );
     };
