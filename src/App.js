2bdba6c8a5831238e030054f86ccf9f09fb12790
Robin Sardja <robinsardja@gmail.com>
2023-10-07 22:00:18 -0400

    Implemented ChatGPT clone YouTube tutorial.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,101 @@
-// const API_KEY = "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"
-
-// async function fetchData() {
-//   const response = await fetch( "https://api.openai.com/v1/chat/completions", {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${API_KEY}`,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       model: "gpt-3.5-turbo",
-//       messages: [{
-//         role: "user",
-//         content: "What is the worst thing about wildfires?",
-//       }],
-//       // max_tokens: 7,
-//     })
-//   })
-
-//   const data = await response.json()
-//   console.log(data)
-// }
-
-// fetchData()
+import { useState, useEffect } from 'react'
 
 const App = () => {
+
+  const [ value, setValue ] = useState(null)
+  const [ message, setMessage ] = useState(null)
+  const [ previousChats, setPreviousChats ] = useState([])
+  const [ currentTitle, setCurrentTitle ] = useState(null)
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if( !currentTitle && value && message ) {
+      setCurrentTitle(value)
+    }
+    if( currentTitle && value && message ) {
+      setPreviousChats(prevChats => (
+        [...prevChats,
+          {
+            title: currentTitle,
+            role: "user",
+            content: value,
+          },
+          {
+            title: currentTitle,
+            role: message.role,
+            content: message.content,
+          },
+        ]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter( previousChat => previousChat.title === currentTitle )
+  const uniqueTitles = Array.from(new Set(previousChats.map( previousChat => previousChat.title )))
+  console.log(uniqueTitles)
+
   return (
     <div className="App">
+      <section className="side-bar">
+        <button onClick={createNewChat}>+ New chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+        </ul>
+        <nav>
+          <p>EmergenSAVE</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>EmergenSAVE</h1>}
+        <ul className="feed">
+          {currentChat.map(( chatMessage, index ) => <li key={index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>🡒</div>
+          </div>
+          <p className="info">
+            Made by Grace Chang, Nathan Cheng, Robin Sardja, and Nicholas Tsai for SASEhack Fall 2023
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
