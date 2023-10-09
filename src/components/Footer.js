3bb97994fbaf5463c8fe59f4c81b2e9b4f8620cb
GrassC575 <61118999+GrassC575@users.noreply.github.com>
2023-10-08 21:19:11 -0400

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer(){
     return (
         <div className='app-footer'>           
-                <p style={{ color: 'white', fontSize: 25}}>Brought to you by <strong>NOODLEZ</strong></p>
-                <p style={{ color: 'white', fontSize: 17}}>Nathan Cheng, Robin Sardja, Grace Chang, Nicholas Tsai</p>
+                <p style={{ color: 'white', fontSize: 25}}>Brought to you by <strong>NOODLEZ</strong> for SASEHack 2023</p>
+                <p style={{ color: 'white', fontSize: 17}}>Grace Chang, Nathan Cheng, Robin Sardja, Nicholas Tsai</p>
         </div>
     )
 }
